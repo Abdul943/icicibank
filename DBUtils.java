e7eb585405dd626ac263ef67e9aab66673adf4fb
Abdul943 <abdulhameed810536@gmail.com>
2026-01-21 19:48:31 +0530

    Update DBUtils.java

--- DBUtils.java
+++ DBUtils.java
@@ -1,6 +1,3 @@
 i want to add one more line here
 This file contains the DB related script..
-<<<<<<< HEAD
-Hello addthis line for updating..
-=======
->>>>>>> parent of 93c15fe (Delete DBUtils.java)
+ parent of 93c15fe (Delete DBUtils.java)
