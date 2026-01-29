38c15004a0d530cbf24b4cd5fdd133141c65762c
Abdul943 <abdulhameed810536@gmail.com>
2026-01-29 18:59:10 +0530

    Delete DBUtils.java

--- DBUtils.java
+++
@@ -1,7 +0,0 @@
-Hello
-i want to add one more line here
-This file contains the DB related script
-Hello addthis line for updatin
-parent of 93c15fe (Delete DBUtils.java
-added this line in dev be7eb585405dd626ac263ef67e9aab66673adf4fb
-i like u
